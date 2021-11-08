3ab0bc41e4108ec09d07a15cfc8f73824c4a5e5b
jimdevops19 <jimdevops@gmail.com>
2021-11-08 16:43:03 +0200

    PublishSecond

---
+++ 02 - Creating Cells & Mines/cell.py
@@ -0,0 +1,24 @@
+from tkinter import Button
+
+
+class Cell:
+    def __init__(self, is_mine=False):
+        self.is_mine = is_mine
+        self.cell_btn_object = None
+
+    def create_btn_object(self, location):
+        btn = Button(
+            location,
+            text="Text"
+        )
+        btn.bind('<Button-1>', self.left_click_actions ) # Left Click
+        btn.bind('<Button-3>', self.right_click_actions ) # Right Click
+        self.cell_btn_object = btn
+
+    def left_click_actions(self, event):
+        print(event)
+        print("I am left clicked!")
+
+    def right_click_actions(self, event):
+        print(event)
+        print("I am right clicked!")
---
+++ 02 - Creating Cells & Mines/main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from cell import Cell
+import settings
+import utils
+
+
+root = Tk()
+# Override the settings of the window
+root.configure(bg="black")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title("Minesweeper Game")
+root.resizable(False, False)
+
+top_frame = Frame(
+    root,
+    bg='black',
+    width=settings.WIDTH,
+    height=utils.height_prct(25)
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg='black',
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25),
+)
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=x, row=y
+        )
+
+
+# Run the window
+root.mainloop()
